3a6a2540bcf870de91c5fc2d383777312920dea0
Amer Shehady <shehadyamer@gmail.com>
2021-06-03 12:19:35 +0300

    Initial commit

---
+++ BubbleSortVisualizer/Commands/RelayCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BubbleSortVisualizer.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Func<Task> execute;
+
+        private readonly Func<bool> canExecute;
+        public RelayCommand(Func<Task> execute) : this(execute, canExecute: null)
+        {
+        }
+
+        public RelayCommand(Func<Task> execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null ? true : this.canExecute();
+        }
+
+        public async void Execute(object parameter)
+        {
+            await this.execute();
+        }
+    }
+}
